902baa78624521fc74ad128ce2bd02398b6a1445
Luis Alberto Suarez <luissuac@gmail.com>
2025-01-28 15:26:48 -0500

    Initial Commit

---
+++ Prueba_Grupo_ASD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba_Grupo_ASD
+//
+//  Created by Luis Alberto Suarez on 28/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Prueba_Grupo_ASDUITests/Prueba_Grupo_ASDUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Prueba_Grupo_ASDUITestsLaunchTests.swift
+//  Prueba_Grupo_ASDUITests
+//
+//  Created by Luis Alberto Suarez on 28/01/25.
+//
+
+import XCTest
+
+final class Prueba_Grupo_ASDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
